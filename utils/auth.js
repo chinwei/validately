4fa4fec82cb30a2f36e40c4998d4ab7c7c27c0a8
Chin Wei <chinwei.w@gmail.com>
2017-06-14 09:36:35 +0800

    added auth.js

---
+++ utils/auth.js
@@ -0,0 +1,78 @@
+import firebase from 'firebase'
+import firebaseui from 'firebaseui'
+import Router from 'vue-router';
+
+// var provider = new firebase.auth.FacebookAuthProvider();
+
+var router = new Router({
+});
+
+export function login() {
+
+    var _this = this;
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  
+}
+
+export function logout() {
+	
+	firebase.auth().signOut().then(function() {
+	
+	router.replace('/');
+
+	}).catch(function(error) {
+
+	});
+}
+
+function checkLoggedIn(result) {
+	console.log(result);
+	return result
+}
+
+export function isLoggedIn(callback) {
+	firebase.auth().onAuthStateChanged(function(user) {
+		callback(user);
+	});
+}
+
+
+isLoggedIn(checkLoggedIn);
+
+
+
+
+function onComplete(a){ // When the code completes, do this
+    alert(a);
+}
+
+function getFive(whenDone){ 
+    var a;
+
+
+
+    setTimeout(function(){
+         a=5;
+         whenDone(a);
+    },10);
+}
+
+
+
